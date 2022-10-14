0225d6573e3349cbcbebbc7b8b68a763a26f016e
IvanDyvak <ivan.dyvak@gmail.com>
2022-10-15 01:47:07 +0300

    Updates

---
+++ precache-manifest.d13cf2ecce2c7c7923fd4721047abbb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f664f9cf0d92b24e6856",
+    "url": "/static/css/main.1a2ed311.chunk.css"
+  },
+  {
+    "revision": "f664f9cf0d92b24e6856",
+    "url": "/static/js/main.f664f9cf.chunk.js"
+  },
+  {
+    "revision": "f15750f70e1a46780683",
+    "url": "/static/js/1.f15750f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf215301538374dd915777f7cda828f9",
+    "url": "/index.html"
+  }
+];
