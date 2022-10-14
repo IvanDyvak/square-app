f0ee2dc248765e5d957b9ddff44a7697468eb5e5
IvanDyvak <ivan.dyvak@gmail.com>
2022-10-14 19:32:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState , useCallback} from 'react';
+import './App.css';
+
+function App() {
+    const [ option, setOption ] = useState('Pick mode');
+    const [ field, setField ] = useState("0");
+    const [ level, setLevel ] = useState([]);
+    const [ activeSquare, setActiveSquare ] = useState('');
+
+
+    useEffect(() => {
+
+        fetch(`https://demo7919674.mockable.io`)
+            .then((response) => response.json())
+            .then((response) => {
+                console.log(response);
+                setLevel(response);
+            })
+
+            .catch((error) => {
+                console.error('Error:', error);
+                setLevel(error);
+            });
+        },
+        []);
+
+    const SquareItem = ({ id }) => (
+                <div
+                    onMouseEnter={useCallback((e) => {
+                        e.target.classList.toggle("active");
+                    }, [id])}
+                    className="squares-list__item"
+                    id={id}>
+                </div>
+    )
+
+    const handleChange = (event) => {
+        setOption(event.target.value);
+        setField(event.target.id);
+        console.log(field);
+    }
+    console.log(document.querySelectorAll('option')[0]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // console.log(option, field);
+    }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/images/logo.png" className="App-logo" alt="logo" />
+      </header>
+        <form onSubmit={handleSubmit}>
+            <label>
+                <select value="Pick mode" onChange={handleChange}>
+                    <option value={option} id={field}>{option}</option>
+                    {level.map(({name, field}) =>
+                        <option value={name} key={name} id={field}>{name}</option>
+                    )}
+                </select>
+            </label>
+            <input type="submit" value="Start" className="init-btn"/>
+        </form>
+        <h2>{field}</h2>
+        <div className="squares-list">
+            {[...Array(15).keys()].map((num) =>
+                <SquareItem
+                    key={num} id={num}
+                    onMouseEnter={setActiveSquare}
+                    isActive={activeSquare} />
+            )}
+        </div>
+    </div>
+  );
+}
+export default App;
