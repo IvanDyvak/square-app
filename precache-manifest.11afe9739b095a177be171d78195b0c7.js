995f76f2d2257716f02b4d48a993a2889ce4c294
IvanDyvak <ivan.dyvak@gmail.com>
2022-11-02 01:40:12 +0200

    Updates

---
+++ precache-manifest.11afe9739b095a177be171d78195b0c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66bd7137ef4597077a89",
+    "url": "/square-app/static/css/main.631b4d99.chunk.css"
+  },
+  {
+    "revision": "66bd7137ef4597077a89",
+    "url": "/square-app/static/js/main.66bd7137.chunk.js"
+  },
+  {
+    "revision": "bfe22cd941e9e1de8b22",
+    "url": "/square-app/static/js/1.bfe22cd9.chunk.js"
+  },
+  {
+    "revision": "2d519aefc10f421bbee8",
+    "url": "/square-app/static/js/runtime~main.2d519aef.js"
+  },
+  {
+    "revision": "71b4260cfa5a513a36930a76e6464bd2",
+    "url": "/square-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/square-app/precache-manifest.8a48d6248ce096c4c9bd1bdd75359bc1.js"
+  "/square-app/precache-manifest.11afe9739b095a177be171d78195b0c7.js"
 );
 
 workbox.clientsClaim();
