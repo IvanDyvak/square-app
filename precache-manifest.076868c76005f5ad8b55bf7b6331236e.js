2ee895d22a82eb6eb948a5eef6e6ec8f990375d8
Ivan <you@example.com>
2022-11-05 00:33:59 +0200

    Updates

--- precache-manifest.076868c76005f5ad8b55bf7b6331236e.js
+++ precache-manifest.076868c76005f5ad8b55bf7b6331236e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "780c67379fc649b83045",
-    "url": "/square-app/static/css/main.30d80e7d.chunk.css"
+    "revision": "97fc4d092e30f51b8f40",
+    "url": "/square-app/static/css/main.1103b3f5.chunk.css"
   },
   {
-    "revision": "780c67379fc649b83045",
-    "url": "/square-app/static/js/main.780c6737.chunk.js"
+    "revision": "97fc4d092e30f51b8f40",
+    "url": "/square-app/static/js/main.97fc4d09.chunk.js"
   },
   {
     "revision": "bfe22cd941e9e1de8b22",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/square-app/static/js/runtime~main.2d519aef.js"
   },
   {
-    "revision": "28639724abd67b3c6c4f9ae5ca91208f",
+    "revision": "cdbb59c19eaa56703c5b0b2352c4ee0f",
     "url": "/square-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/square-app/precache-manifest.c3bc99ce8879af55e405ce0321a5c901.js"
+  "/square-app/precache-manifest.076868c76005f5ad8b55bf7b6331236e.js"
 );
 
 workbox.clientsClaim();
